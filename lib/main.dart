0530eb2dca303eba016158b9aef87f815ec84497
yeapning <ybys1004@naver.com>
2025-03-29 18:29:11 +0900

    feat-6-OAuth-회원가입-로그인-화면-구현

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../routes.dart';
+import 'screens/main_tabs/login/login_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,12 +12,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      initialRoute: '/', // 초기 화면 설정
-      routes: AppRoutes.routes, // routes.dart 파일에서 설정한 경로를 가져오기
+      home: LoginScreen(),
+      // title: 'Flutter Demo',
+      // theme: ThemeData(
+      //   primarySwatch: Colors.blue,
+      // ),
+      // initialRoute: '/', // 초기 화면 설정
+      // routes: AppRoutes.routes, // routes.dart 파일에서 설정한 경로를 가져오기
     );
   }
 }
