eefb310e1c51351f68659af4d63ed84f5cf51fa0
yeapning <ybys1004@naver.com>
2025-04-13 01:45:27 +0900

    feat: 로그인-회원가입-출산방법-선택-화면

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
 import 'routes.dart';
 import 'widgets/layout.dart';
-=======
-import '../routes.dart';
->>>>>>> 686e9ef (RE : feat-6-OAuth-회원가입-로그인-화면-구현)
 
 void main() {
   runApp(const MyApp());
@@ -20,16 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-<<<<<<< HEAD
-<<<<<<< HEAD
       home: const Layout(), // 초기 화면 설정
-=======
-      initialRoute: '/', // 초기 화면 설정
->>>>>>> 686e9ef (RE : feat-6-OAuth-회원가입-로그인-화면-구현)
-=======
-      initialRoute: '/home', // 이후 아래 코드로 대체 예정 우선 개발 중인 화면 띄우기 위함
-      // home: const Layout(), // 초기 화면 설정
->>>>>>> 1955ce7 (Feat: 로그인-회원가입-출산일-출산예정일-선택-화면-구현)
       routes: AppRoutes.routes, // routes.dart 파일에서 설정한 경로를 가져오기
     );
   }
