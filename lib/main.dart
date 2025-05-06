4d708d39cafc670aa9002538e5e53c86c8c7973b
yeapning <ybys1004@naver.com>
2025-05-06 21:49:18 +0900

    Fix: 충돌 해결

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,7 @@ import 'package:flutter/material.dart';
 import '../routes.dart';
 import 'provider/calendar_data.dart';
 import 'package:provider/provider.dart';
-<<<<<<< HEAD
-import 'widgets/layout.dart';
-import 'screens/main_tabs/login/login_user_select_page.dart';
-=======
 import 'screens/home/splash_screen.dart';
->>>>>>> develop
 
 void main() {
   runApp(
@@ -28,14 +23,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-<<<<<<< HEAD
-      home: const LoginUserSelect(),
-      // home: const Layout(), // 초기 화면 설정
-      // routes: AppRoutes.routes, // routes.dart 파일에서 설정한 경로를 가져오기
-=======
       home: const SplashScreen(), // 초기 화면 설정
       routes: AppRoutes.routes, // routes.dart 파일에서 설정한 경로를 가져오기
->>>>>>> develop
     );
   }
 }
--- lib/routes.dart
+++ lib/routes.dart
@@ -4,17 +4,11 @@ import 'screens/home/settings.dart';
 import 'screens/main_tabs/report/report_main_page.dart';
 import 'screens/main_tabs/report/report_detail_page.dart';
 import 'screens/main_tabs/page2.dart';
-import 'screens/main_tabs/login/login_user_select_page.dart';
 
 class AppRoutes {
   static final Map<String, WidgetBuilder> routes = {
-<<<<<<< HEAD
-    // '/home': (context) => const HomePage(),
-    '/home': (context) => const LoginUserSelect(),
-=======
     '/home': (context) => const HomePage(),
-    '/settings' : (context) => const SettingPage(),
->>>>>>> develop
+    '/settings': (context) => const SettingPage(),
     '/calendar': (context) => const ReportMainPage(),
     '/calendarDetail': (context) => const ReportDetailPage(),
     '/chat': (context) => const Page2(),
