3dbae702998839c24d9161f7e2f6c79759df9d79
yeapning <ybys1004@naver.com>
2025-05-10 00:50:30 +0900

    chore:main.dart 코드 수정

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'provider/calendar_data.dart';
 import 'provider/notification_provider.dart';
 import 'package:provider/provider.dart';
 import 'screens/home/splash_screen.dart';
-import 'screens/main_tabs/login/login_user_select_page.dart';
 
 void main() {
   runApp(
@@ -28,8 +27,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: LoginUserSelect(),
-      // home: const SplashScreen(), // 초기 화면 설정
+      home: const SplashScreen(), // 초기 화면 설정
       routes: AppRoutes.routes, // routes.dart 파일에서 설정한 경로를 가져오기
     );
   }
